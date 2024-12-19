31a5d2e03a220f4e32043f23eff7941657c88787
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2024-12-19 15:26:20 +0800

    Update Routes.tsx

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -16,7 +16,7 @@ const RoutesComponent: React.FC = () => {
     <Router>
     <Routes>
       {/* Redirect root to login page */}
-      <Route path="/" element={<Navigate to="/login" replace/>} />
+      <Route path="/" element={<LoginPage />} />
 
       {/* Public Routes */}
       <Route path="/login" element={<LoginPage />} />
